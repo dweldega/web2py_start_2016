884980a7874afd1360f75c1f5a8a98aa4d5a6085
dweldega <dweldega@ucsc.edu>
2016-10-14 12:25:13 -0700

    name display errors on web2py

--- controllers/default.py
+++ controllers/default.py
@@ -22,21 +22,41 @@ def index():
     """
     This is your main controller.
     """
+    posts = None
+    if auth.user_id is not None:
+        # The user is logged in.
+        # Gets the list of all checklists for the user.
+        #posts = db(db.posts.user_email == auth.user.email).select(
+            posts = db().select(db.post.ALL, orderby=~db.post.created_on)
+
+    return dict(posts=posts)
     # I am creating a bogus list here, just to have some divs appear in the
     # view.  You need to read at most 20 posts from the database, in order of
     # most recent first, and you need to return that list here.
     # Note that posts is NOT a list of strings in your actual code; it is
     # what you get from a db(...).select(...).
-    posts = ['banana', 'pear', 'eggplant']
-    return dict(posts=posts)
+    # posts = ['banana', 'pear', 'eggplant']
+
+
 
 
 @auth.requires_login()
 def edit():
-    """
-    This is the page to create / edit / delete a post.
-    """
-    return dict()
+
+    form = SQLFORM(db.post)
+
+    if form.process().accepted:
+        # At this point, the record has already been inserted.
+        session.flash = T('Posted.')
+        redirect(URL('default', 'index'))
+    elif form.errors:
+        session.flash = T('Please enter correct values.')
+        full_name = get_user_name_from_email
+    return dict(form=form, full_name=full_name)
+
+
+
+    #return dict()
 
 
 def user():
